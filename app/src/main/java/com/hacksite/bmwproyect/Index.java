3961e38cd6d72dc8279da9efd3c327cad97453d7
SaulSandovalM <sauldevelop@gmail.com>
2018-04-03 17:27:22 -0500

    Subiendo

---
+++ app/src/main/java/com/hacksite/bmwproyect/Index.java
@@ -0,0 +1,45 @@
+package com.hacksite.bmwproyect;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Index extends AppCompatActivity {
+
+    TextView maxSpeed;
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mConditionRef = mRootRef.child("Velocidad");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+
+        maxSpeed = findViewById(R.id.maxSpeed);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        mConditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                maxSpeed.setText(text);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
