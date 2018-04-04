cd5445f22073ae6430a82c0e456903062f7ea3bb
SaulSandovalM <sauldevelop@gmail.com>
2018-04-04 16:28:53 -0500

    diseño nuevo

--- app/src/main/java/com/hacksite/bmwproyect/Index.java
+++ app/src/main/java/com/hacksite/bmwproyect/Index.java
@@ -2,44 +2,17 @@ package com.hacksite.bmwproyect;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class Index extends AppCompatActivity {
-
-    TextView textView5;
-
-    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference mConditionRef = mRootRef.child("Nombre");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
-
-        textView5 = findViewById(R.id.textView5);
     }
 
     @Override
     protected void onStart(){
         super.onStart();
-
-        mConditionRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String text = dataSnapshot.getValue(String.class);
-                textView5.setText(text);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
     }
 }
