dbf4c91e38570ede61fe64e334c70472406144da
SaulSandovalM <sauldevelop@gmail.com>
2018-04-04 20:54:11 -0500

    consumo basico

--- app/src/main/java/com/hacksite/bmwproyect/Index.java
+++ app/src/main/java/com/hacksite/bmwproyect/Index.java
@@ -2,17 +2,45 @@ package com.hacksite.bmwproyect;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Index extends AppCompatActivity {
+
+    TextView mvelocidad, maceleracion, mdistancIA, mbateria;
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mvelocidadRef = mRootRef.child("aceleracion/" +Id);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+
+        mvelocidad = (TextView)findViewById(R.id.text1);
+
     }
 
     @Override
     protected void onStart(){
         super.onStart();
+
+        mvelocidadRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                mvelocidad.setText(text);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
